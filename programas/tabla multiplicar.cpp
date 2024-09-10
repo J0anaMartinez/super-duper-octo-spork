e823858a1b055be4b1d4014351f3b611a4d59ac2
JMartinez <joananatalymartinez@gmail.com>
2024-09-10 15:46:51 -0300

    pide al usuario ingresar un dato mayor a 0

--- programas/tabla multiplicar.cpp
+++ programas/tabla multiplicar.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int main (){
 
-int numero = 5;
+int numero;
 
 for(int i = 1; i<= 10; i++){
 
     cout << numero << "x" << i << "=" << numero * i << endl;
     cout << "ingrese un numero: ";
     cin >> numero;
+
+     if(numero <= 0){
+    cout << "ingrese un numero mayor a cero" << endl;
+  }
 
 }
 
